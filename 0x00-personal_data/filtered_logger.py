9a74ae4e23a6f46b5d0acc622e93943c9eefab98
mahmoud malek <information.mahmoud@gmail.com>
2024-09-12 23:01:43 +0300

    add\udpate files :fire:

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,5 +9,6 @@ def filter_datum(fields: set, redaction: str, message: str,
                  separator: str) -> str:
     """ a function to obfuscate the important data """
     pattern = '|'.join([f'{field}=[^{separator}]+' for field in fields])
-    return re.sub(
-        pattern, lambda match: f'{match.group(0).split("=")[0]}={redaction}', message)
+    return re.sub(pattern,
+                  lambda match: f'{match.group(0).split("=")[0]}={redaction}',
+                  message)
