eb3129a695ec113dc179fc70b0c0a72f5dae3f5e
mahmoud malek <information.mahmoud@gmail.com>
2024-09-19 00:08:10 +0300

    add\udpate files :fire:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,7 +14,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """ extract_base64_authorization_header
-                        """
+                                        """
         if not authorization_header or type(authorization_header) is not str:
             return None
         if not authorization_header.startswith("Basic "):
@@ -23,7 +24,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """ decode_base64_authorization_header
-                        """
+                                        """
         if not base64_authorization_header:
             return None
 
@@ -31,6 +32,7 @@ class BasicAuth(Auth):
             return None
 
         try:
-            return base64_authorization_header.encode('utf-8').decode('base64')
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
         except Exception:
             return None
