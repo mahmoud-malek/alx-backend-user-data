7d84ac299f491482637a1e6501ef6ba3eb77cb20
mahmoud malek <information.mahmoud@gmail.com>
2024-09-19 00:01:07 +0300

    add\udpate files :fire:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,9 +13,21 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """ extract_base64_authorization_header
-        """
+                """
         if not authorization_header or type(authorization_header) is not str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode_base64_authorization_header
+                """
+        if not base64_authorization_header or \
+           type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64_authorization_header.encode('utf-8').decode('base64')
+        except Exception:
+            return None
