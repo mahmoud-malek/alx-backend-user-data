b90dd1fe79da96df8c27b2590fe4b5785583801a
mahmoud malek <information.mahmoud@gmail.com>
2024-09-18 20:31:42 +0300

    add\udpate files :fire:

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,13 +21,13 @@ if AUTH_TYPE == "auth":
 
 @app.before_request
 def before_request():
-    "" "Before request" ""
-
+    """ Before request
+    """
     if auth is None:
         return
-    if auth.require_auth(request.path, ['/api/v1/status/',
-                                        '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/']):
+    if auth.require_auth(
+        request.path,
+            ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
         if not auth.authorization_header(request):
             abort(401)
         if not auth.current_user(request):
