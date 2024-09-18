79df89a4685e245551e3af06c03f326aad6764a1
mahmoud malek <information.mahmoud@gmail.com>
2024-09-18 23:58:51 +0300

    add\udpate files :fire:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,15 +10,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class for the API """
 
-     def extract_base64_authorization_header(self,
-                                              authorization_header: str) -> str:
-          """ extract_base64_authorization_header
-            """
-           if authorization_header is None:
-                return None
-            if not isinstance(authorization_header, str):
-                return None
-            if not authorization_header.startswith('Basic '):
-                return None
-
-            return authorization_header[6:]
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract_base64_authorization_header
+        """
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
