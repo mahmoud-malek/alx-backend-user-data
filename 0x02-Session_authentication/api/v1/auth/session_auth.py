2b309fa8467ac8a04727298c48e077d04dfd8049
mahmoud malek <information.mahmoud@gmail.com>
2024-09-19 22:28:47 +0300

    add\udpate files :fire:

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,3 +31,16 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         from models.user import User
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ deletes the user session / logout """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
