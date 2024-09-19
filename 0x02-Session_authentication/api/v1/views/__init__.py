d95dc4b058196e7a036b167e63cc59301248a6ca
mahmoud malek <information.mahmoud@gmail.com>
2024-09-19 22:28:36 +0300

    add\udpate files :fire:

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -9,5 +9,6 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *  # noqa: E402
 from api.v1.views.users import *  # noqa: E402
+from api.v1.views.session_auth import *  # noqa: E402
 
 User.load_from_file()
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+""" Session authentication views """
+
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session_login() -> str:
+    """ POST /api/v1/auth_session/login
+            Return:
+                    - User object JSON represented
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return jsonify({"error": "email or password missing"}), 400
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+        session_id = auth.create_session(user.id)
+        response = jsonify(user.to_json())
+        response.set_cookie(getenv('SESSION_NAME'), session_id)
+        return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def auth_session_logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+            Return:
+                    - empty JSON
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
