c7d58d4a5249fa88c4f0bcb45246eaf2943641c6
mahmoud malek <information.mahmoud@gmail.com>
2024-09-19 21:50:35 +0300

    add\udpate files :fire:

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,10 +31,12 @@ def before_request():
     """
     if auth is None:
         return
-    if auth.require_auth(
-            request.path, ['/api/v1/status/', '/api/v1/unauthorized/',
-                           '/api/v1/forbidden/']):
-        if not auth.authorization_header(request):
+    if auth.require_auth(request.path, [
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+    ]):
+        if not auth.authorization_header(request) and \
+                not auth.session_cookie(request):
             abort(401)
 
         user = auth.current_user(request)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,16 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
