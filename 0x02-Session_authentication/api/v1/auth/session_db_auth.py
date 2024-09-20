29efb18a894d8b3751d5b1ddd5fe8ff1ee8ab19b
mahmoud malek <information.mahmoud@gmail.com>
2024-09-20 03:58:09 +0300

    add\udpate files :fire:

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,63 +1,57 @@
 #!/usr/bin/env python3
+"""
+Defines Session Auth class that uses a file storage DB
+"""
+from datetime import datetime, timedelta
+from typing import Union
 
-""" defines the user authentication model for db storage """
+from models.user_session import UserSession
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
-from api.v1.auth.session_auth import SessionAuth
+
+class SessionDBAuth(SessionExpAuth):
+    """
+    Database Session Authentication implementation
+    """
+
+    def create_session(self, user_id=None) -> Union[str, None]:
+        """
+        Create session id from random string
+        """
 
 
-class SessionDBAuth(SessionAuth):
-    """ SessionDBAuth class """
-
-    def create_session(self, user_id=None):
-        """ Create a session """
+if user_id is None or not isinstance(user_id,         if user_id is None or )            return None
         session_id = super().create_session(user_id)
-        if session_id is None:
-            return None
-
-        from models.user_session import UserSession
-        user_session = UserSession(user_id=user_id, session_id=session_id)
-        user_session.save()
-
+        if session_id:
+            kwargs = {'user_id': user_id, 'session_id': session_id}
+            new_user_session = UserSession(**kwargs)
+            new_user_session.save()
         return session_id
 
-    def user_id_for_session_id(self, session_id=None):
-        """ User ID for Session ID """
-        if session_id is None:
-            return None
+    def user_id_for_session_id(self, session_id=None) -> Union[str, None]:
+        """
+        Get user id based on session id from DB
+        """
+        users_session = UserSession.search({'session_id': session_id})
+        if users_session != []:
+            if not (timedelta(seconds=self.session_duration) +
+                    users_session[0].created_at <= datetime.now()):
+                return users_session[0].user_id
+        return None
 
-        from models.user_session import UserSession
-        try:
-            user_session = UserSession.search({'session_id': session_id})
-        except Exception:
-            return None
-
-        if not user_session:
-            return None
-
-        if self.session_duration <= 0:
-            return user_session[0].user_id
-
-        if (user_session[0].created_at +
-                self.session_duration) < datetime.now():
-            return None
-
-        return user_session[0].user_id
-
-    def destroy_session(self, request=None):
-        """ Delete a session / logout """
+    def destroy_session(self, request=None) -> bool:
+        """
+        Deletes the user session from DB / logout user
+        """
         if request is None:
             return False
-
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
-
-        from models.user_session import UserSession
-        user_session = UserSession.search({'session_id': session_id})
-
-        if not user_session:
+        if self.user_id_for_session_id(session_id) is None:
             return False
-
-        user_session[0].remove()
-
-        return True
+        usersession = UserSession.search({'session_id': session_id})
+        if usersession != []:
+            usersession[0].remove()
+            return True
+        return False
