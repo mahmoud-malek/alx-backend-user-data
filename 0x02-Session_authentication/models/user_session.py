9d7e03a22b39d4e31bcf48ae2f51551d514e533c
mahmoud malek <information.mahmoud@gmail.com>
2024-09-20 03:58:57 +0300

    ok

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-
-""" defines the user session model for db storage """
-
+"""
+Defines a UserSession class
+"""
+from datetime import datetime
 
 from models.base import Base
 
 
 class UserSession(Base):
-    """ UserSession class """
+    """
+    User session class
+    """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """ initializes a user session """
+        """
+        Instialize a User Session instance
+        """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
+        self.created_at = datetime.now()
