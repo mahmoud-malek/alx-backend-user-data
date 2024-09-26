01645a66a54eecd20012ae181c84ffa5e2065c9f
mahmoud malek <information.mahmoud@gmail.com>
2024-09-26 17:29:15 +0300

    add\udpate files :fire:

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-	Defines a User class for authentication
+    Defines a User class for authentication
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
