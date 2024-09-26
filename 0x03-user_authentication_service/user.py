0294c0cc0fa4498ac8475ad1559211c9ba1b4e63
mahmoud malek <information.mahmoud@gmail.com>
2024-09-26 17:28:04 +0300

    add\udpate files :fire:

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-
-""" the User module defines a User class """
-
-from sqlalchemy import Column, String, Integer, DateTime
+"""
+	Defines a User class for authentication
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """ class user defines the functionality of the user """
+    """
+    User authentication class
+    """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, autoincrement="auto", primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
