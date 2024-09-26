c17edbc96915203e87ba59e48b88028640439fc8
mahmoud malek <information.mahmoud@gmail.com>
2024-09-26 21:00:03 +0300

    done :hammer:

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -15,11 +17,11 @@ from typing import TypeVar
 
 class DB:
     """DB class
-    """
+        """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
-        """
+                """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -28,7 +30,7 @@ class DB:
     @property
     def _session(self) -> Session:
         """Memoized session object
-        """
+                """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -36,8 +38,20 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the data base
-        """
+                """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> TypeVar('User'):
+        """ Finds a user by a given attribute
+                """
+        if not kwargs:
+            raise InvalidRequestError
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound
+
         return user
